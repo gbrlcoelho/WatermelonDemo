3db4cae3c4f2102ac2f93f2972818c7bf83af7dc
gccano <gccano@viptech.com.br>
2022-08-02 10:09:36 -0300

    feat(global): first commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 150,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: true,
+  bracketSpacing: false,
+};
